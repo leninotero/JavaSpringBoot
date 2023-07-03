387b340dc1fc56aba5085e8e5c8d7584f2667855
leninotero <leerabot@gmail.com>
2023-07-02 22:45:09 -0300

    add pagina de pesquisa de titulos

--- algaworks/src/main/java/cobranca/algaworks/controller/TituloController.java
+++ algaworks/src/main/java/cobranca/algaworks/controller/TituloController.java
@@ -33,6 +33,10 @@ public class TituloController {
         return mv;
     }
 
+    @RequestMapping
+    public String pesquisar(){
+        return "PesquisaTitulos";
+    }
     @ModelAttribute("todosStatusTitulo")
     public List<StatusTitulo> todosStatusTitulo(){
         return Arrays.asList(StatusTitulo.values());
